7fde888e2196efa0e5995a428b1466798a4e6b9d
cra1gster <craig.a.amos@gmail.com>
2016-07-14 23:59:06 +0700

    Update template-ttrssResults.php

--- template-ttrssResults.php
+++ template-ttrssResults.php
@@ -3,16 +3,12 @@
 ?>
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Codilight_Lite
+
+Custom template derived from page.php
+Website uses the Codilight theme
+
+Pulls db query using dbinfo.php ; allows param for resultset restriction
+
  */
 
 get_header(); ?>
