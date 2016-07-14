daed18c7a3e315f06adb4309186c5d8a75c89f77
cra1gster <craig.a.amos@gmail.com>
2016-07-15 02:28:16 +0700

    Update nw_search.js

--- nw_search.js
+++ nw_search.js
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
+  
+  // Hate the 'alert' สันหลังยา
   <title>dialog demo</title>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
